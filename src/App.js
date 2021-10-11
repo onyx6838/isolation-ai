655235cd6168b3867e9b97ef4c9da86cb3ab457e
onyx6838 <giang.mink@gmail.com>
2021-10-11 16:55:41 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import IsolationContainer from './components/isolationContainer';
+import './css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Isolation Game</h1>
+      <IsolationContainer width="3" height="3"></IsolationContainer>
+      <div id="graph">
+
+      </div>
     </div>
   );
 }
---
+++ src/components/isolationContainer.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import StrategyManager from '../managers/strategyManager';
+import HeuristicManager from '../managers/heuristicManager';
+import Isolation from './isolation';
+import '../css/style.css';
+
+class IsolationContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      strategy: props.strategy || StrategyManager.minimax,
+      heuristic: props.heuristic || HeuristicManager.offensiveToDefensive,
+      width: props.width || 3,
+      height: props.height || 3,
+      treeDepth: props.treeDepth || 25,
+      miniMaxDepth: props.miniMaxDepth || 6,
+    };
+
+    this.onStrategy = this.onStrategy.bind(this);
+    this.onHeuristic = this.onHeuristic.bind(this);
+    this.onWidth = this.onWidth.bind(this);
+    this.onHeight = this.onHeight.bind(this);
+    this.onTreeDepth = this.onTreeDepth.bind(this);
+    this.onMiniMaxDepth = this.onMiniMaxDepth.bind(this);
+  }
+
+  onStrategy(e) {
+    this.setState({ strategy: StrategyManager[e.currentTarget.value] });
+    console.log(`Strategy set to ${e.currentTarget.value}.`);
+  }
+
+  onHeuristic(e) {
+    this.setState({ heuristic: HeuristicManager[e.currentTarget.value] });
+    console.log(`Heuristic set to ${e.currentTarget.value}.`);
+  }
+
+  onWidth(e) {
+    this.setState({ width: e.currentTarget.value });
+  }
+
+  onHeight(e) {
+    this.setState({ height: e.currentTarget.value });
+  }
+
+  onTreeDepth(e) {
+    this.setState({ treeDepth: e.currentTarget.value });
+  }
+
+  onMiniMaxDepth(e) {
+    this.setState({ miniMaxDepth: e.currentTarget.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <Isolation width={this.state.width} height={this.state.height} treeDepth={this.state.treeDepth} miniMaxDepth={this.state.miniMaxDepth} strategy={this.state.strategy} heuristic={this.state.heuristic}></Isolation>
+
+        <div className="gamePlayOptions mt-3">
+          <div className='row'>
+            <div className='col text-muted'>
+              Game Play
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <input type="radio" name="strategy" value="minimax" checked={this.state.strategy === StrategyManager.minimax} onChange={this.onStrategy} /> <span>Minimax</span>
+              <input type="radio" name="strategy" value="random" checked={this.state.strategy === StrategyManager.random} onChange={this.onStrategy} /> <span>Random</span>
+              <input type="radio" name="strategy" value="none" checked={!this.state.strategy || this.state.strategy === StrategyManager.none} onChange={this.onStrategy} /> <span>2 Players</span>
+            </div>
+          </div>
+          <div className='row mt-2'>
+            <div className='col text-muted'>
+              AI Tactic
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <input type="radio" name="heuristic" value="simple" checked={this.state.heuristic === HeuristicManager.simple} onChange={this.onHeuristic} /> <span>Simple</span>
+              <input type="radio" name="heuristic" value="offensive" checked={this.state.heuristic === HeuristicManager.offensive} onChange={this.onHeuristic} /> <span>Offensive</span>
+              <input type="radio" name="heuristic" value="defensive" checked={this.state.heuristic === HeuristicManager.defensive} onChange={this.onHeuristic} /> <span>Defensive</span>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <input type="radio" name="heuristic" value="aggressive" checked={this.state.heuristic === HeuristicManager.aggressive} onChange={this.onHeuristic} /> <span>Aggressive</span>
+              <input type="radio" name="heuristic" value="offensiveToDefensive" checked={this.state.heuristic === HeuristicManager.offensiveToDefensive} onChange={this.onHeuristic} /> <span>Offensive to Defensive</span>
+              <input type="radio" name="heuristic" value="defensiveToOffensive" checked={this.state.heuristic === HeuristicManager.defensiveToOffensive} onChange={this.onHeuristic} /> <span>Defensive to Offensive</span>
+            </div>
+          </div>
+          <div className='row mt-2'>
+            <div className='col text-muted'>
+              Grid Size
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-auto'>
+              <input type="number" id="width" name="width" value={this.state.width} onChange={this.onWidth} />
+            </div>
+            <div className='col-auto'>
+              <input type="number" id="height" name="height" value={this.state.height} onChange={this.onHeight} />
+            </div>
+          </div>
+          <div className='row mt-2'>
+            <div className='col text-muted'>
+              Minimax Depth
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <input type="number" id="miniMaxDepth" name="miniMaxDepth" value={this.state.miniMaxDepth} onChange={this.onMiniMaxDepth} />
+            </div>
+          </div>
+          <div className='row mt-2'>
+            <div className='col text-muted'>
+              Tree Depth
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col'>
+              <input type="number" id="treeDepth" name="treeDepth" value={this.state.treeDepth} onChange={this.onTreeDepth} />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default IsolationContainer;
