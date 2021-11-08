2769c3e8df9d8fb536acf8c60e3f073f4cc87d94
LanHuongUTC <lanhuong23042000@gmail.com>
2021-11-08 21:24:37 +0700

    huong up

--- web/src/components/isolationContainer.js
+++ web/src/components/isolationContainer.js
@@ -9,19 +9,19 @@ class IsolationContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            roomCode: this.props.roomCode||0,
-            userName: props.userName||'',
-            showHeuristic: props.strategy === StrategyManager.none? false:true,
+            roomCode: this.props.roomCode || 0,
+            userName: props.userName || '',
+            showHeuristic: props.strategy === StrategyManager.none ? false : true,
             strategy: props.strategy || StrategyManager.none,
             heuristic: props.heuristic || HeuristicManager.simple,
             width: props.width || 4,
             height: props.height || 4,
-            treeDepth: props.treeDepth || 30,
-            miniMaxDepth: props.miniMaxDepth || 7
+            treeDepth: props.treeDepth || 15,
+            miniMaxDepth: props.miniMaxDepth || 5
         };
         console.log(this.state.userName);
     }
-  
+
     // onStrategy = (e) => {
     //     this.setState({ strategy: StrategyManager[e.currentTarget.value] });
     //     console.log(`Strategy set to ${e.currentTarget.value}.`);
@@ -37,69 +37,69 @@ class IsolationContainer extends React.Component {
     }
 
     render() {
-            return (
-                <div className="wrapper">
-                        <Isolation width={this.state.width}
-                            roomCode = {this.state.roomCode}
-                            userName = {this.state.userName}
-                            height={this.state.height}
-                            strategy={this.state.strategy}
-                            treeDepth={this.state.treeDepth}
-                            miniMaxDepth={this.state.miniMaxDepth}
-                            heuristic={this.state.heuristic} />
-                            {this.state.showHeuristic && <div className="gamePlayOptions mt-3">
-                                <div className='row'>
-                                    <div className='col text-muted'>
-                                        Game Play
-                                    </div>
-                                </div>
-                                <div className='row mt-2'>
-                                    <div className='col text-muted'>
-                                        AI Tactical
-                                    </div>
-                                </div>
-                                {/* list tactic */}
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="radio" name="heuristic" value="simple" checked={this.state.heuristic === HeuristicManager.simple} onChange={this.onHeuristic} /> <span>Simple</span>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="radio" name="heuristic" value="offensive" checked={this.state.heuristic === HeuristicManager.offensive} onChange={this.onHeuristic} /> <span>Offensive</span>
-                                    </div>
-                                </div>
-                                <div className='row'>
-                                    <div className="col">
-                                        <input type="radio" name="heuristic" value="defensive" checked={this.state.heuristic === HeuristicManager.defensive} onChange={this.onHeuristic} /> <span>Defensive</span>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="radio" name="heuristic" value="aggressive" checked={this.state.heuristic === HeuristicManager.aggressive} onChange={this.onHeuristic} /> <span>Aggressive</span>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="col">
-                                        <input type="radio" name="heuristic" value="offensiveToDefensive" checked={this.state.heuristic === HeuristicManager.offensiveToDefensive} onChange={this.onHeuristic} /> <span>Offensive to Defensive</span>
-                                    </div>
-                                </div>
-                                <div className='row'>
-                                    <div className='col'>
-                                        <input type="radio" name="heuristic" value="defensiveToOffensive" checked={this.state.heuristic === HeuristicManager.defensiveToOffensive} onChange={this.onHeuristic} /> <span>Defensive to Offensive</span>
-                                    </div>
-                                </div>
-                                <div className='row'>
-                                    <div className='col'>
-                                        <input type="radio" name="heuristic" value="walls" checked={this.state.heuristic === HeuristicManager.walls} onChange={this.onHeuristic} /> <span>Walls</span>
-                                    </div>
-                                </div>
-                            </div>}
+        return (
+            <div className="wrapper">
+                <Isolation width={this.state.width}
+                    roomCode={this.state.roomCode}
+                    userName={this.state.userName}
+                    height={this.state.height}
+                    strategy={this.state.strategy}
+                    treeDepth={this.state.treeDepth}
+                    miniMaxDepth={this.state.miniMaxDepth}
+                    heuristic={this.state.heuristic} />
+                {this.state.showHeuristic && <div className="gamePlayOptions mt-3">
+                    <div className='row'>
+                        <div className='col text-muted'>
+                            Game Play
+                        </div>
                     </div>
-            );
-        }
-       
-    
+                    <div className='row mt-2'>
+                        <div className='col text-muted'>
+                            AI Tactical
+                        </div>
+                    </div>
+                    {/* list tactic */}
+                    <div className="row">
+                        <div className="col">
+                            <input type="radio" name="heuristic" value="simple" checked={this.state.heuristic === HeuristicManager.simple} onChange={this.onHeuristic} /> <span>Simple</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <input type="radio" name="heuristic" value="offensive" checked={this.state.heuristic === HeuristicManager.offensive} onChange={this.onHeuristic} /> <span>Offensive</span>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className="col">
+                            <input type="radio" name="heuristic" value="defensive" checked={this.state.heuristic === HeuristicManager.defensive} onChange={this.onHeuristic} /> <span>Defensive</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <input type="radio" name="heuristic" value="aggressive" checked={this.state.heuristic === HeuristicManager.aggressive} onChange={this.onHeuristic} /> <span>Aggressive</span>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <input type="radio" name="heuristic" value="offensiveToDefensive" checked={this.state.heuristic === HeuristicManager.offensiveToDefensive} onChange={this.onHeuristic} /> <span>Offensive to Defensive</span>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='col'>
+                            <input type="radio" name="heuristic" value="defensiveToOffensive" checked={this.state.heuristic === HeuristicManager.defensiveToOffensive} onChange={this.onHeuristic} /> <span>Defensive to Offensive</span>
+                        </div>
+                    </div>
+                    {/* <div className='row'>
+                        <div className='col'>
+                            <input type="radio" name="heuristic" value="walls" checked={this.state.heuristic === HeuristicManager.walls} onChange={this.onHeuristic} /> <span>Walls</span>
+                        </div>
+                    </div> */}
+                </div>}
+            </div>
+        );
+    }
+
+
 }
 
 export default IsolationContainer;
